72fbe8c76cb55c4959f5a157b40fed2612e692c3
kelvinSeamount <kelvinnwachukwu89@gmail.com>
2026-06-28 15:07:49 +0200

    fix: ensure force_delete is set to false for ECR repository

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -3,6 +3,7 @@ resource "aws_ecr_repository" "main" {
 
   name                 = each.value
   image_tag_mutability = "MUTABLE"
+  force_delete         = false
 
   image_scanning_configuration {
     scan_on_push = true
